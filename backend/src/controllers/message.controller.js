ac387f4c1d9a91741522527407a22ab6c1a21336
Obasi <bigmotion001@gmail.com>
2025-03-12 17:30:08 -0700

    add message retrieval and sending functionality; update routes for message handling

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,5 +1,9 @@
 import User from "../models/user.model.js";
+import Message from "../models/message.model.js";
+import  cloudinary from "../lib/cloudinary.js";
 
+
+//get a user for the sidebar
 export const getUsersForSideBar = async(req, res)=>{
     try {
         //login user id
@@ -10,6 +14,81 @@ export const getUsersForSideBar = async(req, res)=>{
         res.status(200).json(filteredUser);
 
     } catch (error) {
+        console.log("error  in getting sidebar user message  controller", error);
+        return res
+          .status(500)
+          .json({ success: false, message: "Internal Server Error" });
+        
+    }
+}
+
+//get our messages
+export const getMessages = async(req, res) =>{
+    try {
+        //our receiver id
+        const {id: userToChatId} = req.params.id;
+        const senderId = req.userId;
+
+        //find messages
+        const messages = await Message.find({
+            $or:[
+                {senderId:senderId, receiverId:userToChatId},
+                {senderId:userToChatId, receiverId:senderId}
+            ]
+        })
+        res.status(200).json(messages);
+        
+    } catch (error) {
+        console.log("error  in getting messages message  controller", error);
+        return res
+          .status(500)
+          .json({ success: false, message: "Internal Server Error" });
+        
+    }
+}
+
+//send message
+export const sendMessage = async(req, res)=>{
+    try {
+        const {text, image} = req.body;
+        //receiver id
+        const {id: receiverId} = req.params.id;
+        //check if receiver id exist
+        const receiver = await User.findById(receiverId);
+        if(!receiver)return res.status(400).json({success:false, message: "Invalid Receiver Id"});
+        //sender id
+        const senderId = req.userId;
+         //check if receiver id exist
+         const my= await User.findById(senderId);
+         if(!my)return res.status(400).json({success:false, message: "Invalid Sender Id"});
+
+         //if user send image as message
+         let imageURL;
+         if(image){
+            //upload to cloudinary
+            const uploadResponse = await cloudinary.uploader.upload(image);
+            imageURL = uploadResponse.secure_url;
+         }
+
+         //create a message
+         const newMessage = new Message({
+            senderId,
+            receiverId,
+            text,
+            image: imageURL
+         });
+         //save
+         await newMessage.save();
+         //todo: realtime functionality goes here => socket.io
+
+         res.status(201).json(newMessage);
+
+        
+    } catch (error) {
+        console.log("error  in sending messages in message  controller", error);
+        return res
+          .status(500)
+          .json({ success: false, message: "Internal Server Error" });
         
     }
 }
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { verifyToken } from "../middlewares/verifyToken.js";
-import {getUsersForSideBar} from "../controllers/message.controller.js";
+import {getUsersForSideBar, getMessages, sendMessage} from "../controllers/message.controller.js";
 const router = express.Router();
 
 
 router.get("/users", verifyToken, getUsersForSideBar);
+router.get("/:id", verifyToken, getMessages);
+router.post("/send/:id", verifyToken, sendMessage);
 
 
 
