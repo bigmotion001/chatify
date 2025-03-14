91d6522100195e80824bfc816f2180e3864a6c4c
Obasi <bigmotion001@gmail.com>
2025-03-13 19:44:58 -0700

    completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,6 +6,7 @@ import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 import {app, server} from "./lib/socket.js";
 
 
@@ -13,21 +14,35 @@ dotenv.config();
 app
 const PORT = process.env.PORT;
 
+
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({origin:"http://localhost:5173", credentials:true}))
 
 
 
-
+const __dirname = path.resolve();
 
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 
-app.get("/", (req, res) => {
-  res.send("Chatify backend is running");
-});
+
+
+
+if(process.env.NODE_ENV==="production"){
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res)=>{
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  })
+}
+
+
+
+
+
 
 server.listen(PORT, () => {
   connectDB();
